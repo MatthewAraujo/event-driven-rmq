395b6441671b12a4beb1e5d067cd6b4b35b72fb5
MatthewAraujo <matthewaraujo20@gmail.com>
2024-07-02 15:54:52 -0300

    send and undestanding exchanges

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"time"
 
 	"github.com/MatthewAraujo/event-driven-rmq/internal"
+	"github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
@@ -25,6 +27,33 @@ func main() {
 	}
 
 	if err := rabbitClient.CreateQueue("customers_test", false, true); err != nil {
+		panic(err)
+	}
+
+	if err := rabbitClient.CreateBinding("customers_created", "customers.created.*", "customer_events"); err != nil {
+		panic(err)
+	}
+
+	if err := rabbitClient.CreateBinding("customers_test", "customers.*", "customer_events"); err != nil {
+		panic(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	if err := rabbitClient.Send(ctx, "customer_events", "customers.created.us", amqp091.Publishing{
+		ContentType:  "text/plain",
+		DeliveryMode: amqp091.Persistent,
+		Body:         []byte(`an cool message between customers`),
+	}); err != nil {
+		panic(err)
+	}
+
+	if err := rabbitClient.Send(ctx, "customer_events", "customers.test", amqp091.Publishing{
+		ContentType:  "text/plain",
+		DeliveryMode: amqp091.Transient,
+		Body:         []byte(`an uncool undurable message`),
+	}); err != nil {
 		panic(err)
 	}
 
--- internal/rabbitmq.go
+++ internal/rabbitmq.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"context"
 	"fmt"
 
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -37,4 +38,36 @@ func (rc RabbitClient) Close() error {
 func (rc RabbitClient) CreateQueue(queueName string, durable, autodelete bool) error {
 	_, err := rc.ch.QueueDeclare(queueName, durable, autodelete, false, false, nil)
 	return err
+}
+
+// CreateBinding will bind the current channel to given exchange using the routingkey provided
+func (rc RabbitClient) CreateBinding(name, binding, exchange string) error {
+	return rc.ch.QueueBind(name, binding, exchange, false, nil)
+}
+
+// send is used to publish pauload onto an exchange with a routing key
+func (rc RabbitClient) Send(ctx context.Context, exchange, routingKey string, options amqp.Publishing) error {
+	return rc.ch.PublishWithContext(
+		ctx,
+		exchange,
+		routingKey,
+		//mandatory is used to determine if an error should be returned upon failure
+		true,
+		//immediate is used to determine if the message should be returned to the consumer immediately
+		// immediate Removed in MQ 3
+		false,
+		options,
+	)
+}
+
+func (rc RabbitClient) Consume(queue, consumer string, autoAck bool) (<-chan amqp.Delivery, error) {
+	return rc.ch.Consume(
+		queue,
+		consumer,
+		autoAck,
+		false,
+		false,
+		false,
+		nil,
+	)
 }
